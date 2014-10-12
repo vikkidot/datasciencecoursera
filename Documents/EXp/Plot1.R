f1b36d27df39478cc6eba260c2cb10e340b8e7da
vikkidot <vikkidot@gmail.com>
2014-10-12 16:07:02 -0700

    try

---
+++ Documents/EXp/Plot1.R
@@ -0,0 +1,9 @@
+data <- read.table("household_power_consumption.txt",sep=";"
+                   ,header=TRUE,na.strings ="?",stringsAsFactors=FALSE)
+data$DateTime <- paste(data$Date, data$Time)
+##data$DateTime <- paste("16/12/2006", "17:24:00")
+data$DateTime <- as.Date(data$DateTime,'%d/%m/%Y %H:%M:%S')
+finaldata <- filter(data, DateTime >= as.Date("2007-02-01 00:00:00"),DateTime < as.Date("2007-02-03 00:00:00"))
+hist(finaldata$Global_active_power, col = "red" , xlab = "Global Active Power(kilowatts)",main = "Global Active Power")
+dev.copy(png,file="plot1.png",height=480,width=480)
+dev.off()
